99390d127755242e229746787716210f7ed20eea
Mary190183 <pma190183@yandex.ru>
2022-07-15 22:25:13 +0300

    init: create

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
+  const check = (x, y = bracketsConfig.map(i => i.join('')).filter(i => x.includes(i))) => {
+    if (!y.length) return false
+    for (let i = 0; i < y.length; i++) {
+        x = x.replace(y[i], '')
+    }
+    return !x ? true : check(x)
+}
+return check(str)
+
   // your solution
 }
